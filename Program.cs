82d8b11546963ce72cba5fd695ee72ada1b53804
AlexB135 <80590603+AlexB135@users.noreply.github.com>
2024-11-25 17:40:43 +0200

    Update Program.cs
    Add GreetUser function to Program.cs

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        GreetUser("User");
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
     }
 }
